4a11add78935f07114cbafbfe261f4c322301128
Martin Brochhaus <mbrochh@gmail.com>
2018-03-19 20:28:50 +0800

    Added edu-summit and young-coders pages

---
+++ src/components/ULListStyleNone.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const ULListStyleNone = styled.ul`
+  max-width: 600px;
+  list-style-type: none;
+  & > li {
+    margin-bottom: 1em;
+  }
+`
+
+export default ULListStyleNone
