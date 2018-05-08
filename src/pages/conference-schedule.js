f306c333e756aa40d7e197dcfdea0611714eda00
Martin Brochhaus <mbrochh@gmail.com>
2018-05-08 21:29:22 +0800

    Adds small style changes for conference-schedule so that limited space on small mobile devices is better utilised...
    * turns tab nav links blue-ish so that they are easier to spot when scrolling down the schedule
    * lots of code-reformatting because of prettier - sorry!

--- src/pages/conference-schedule.js
+++ src/pages/conference-schedule.js
@@ -1,9 +1,29 @@
 import Helmet from 'react-helmet'
 import React from 'react'
+import styled from 'styled-components'
 
-import { Table, TabContent, TabPane, Nav, NavItem, NavLink, Card, Button, CardTitle, CardText, Row, Col } from 'reactstrap';
-import classnames from 'classnames';
+import {
+  Table,
+  TabContent,
+  TabPane,
+  Nav,
+  NavItem,
+  NavLink,
+  Card,
+  Button,
+  CardTitle,
+  CardText,
+  Row,
+  Col,
+} from 'reactstrap'
+import classnames from 'classnames'
 import ContentCard from '../components/ContentCard.js'
+
+const StyledCard = styled(Card)`
+  @media (max-width: 979px) {
+    padding: 0.5em;
+  }
+`
 
 const commonSchedule = {
   day2: {
@@ -18,7 +38,7 @@ const commonSchedule = {
       {
         time: '9:00am - 10:00am',
         speaker: 'Katharine Jarmul, Founder at kjamistan',
-        title: 'Opening Keynote'
+        title: 'Opening Keynote',
       },
       {
         time: '10:00am - 10:40am',
@@ -31,7 +51,7 @@ const commonSchedule = {
       {
         time: '5:15pm - 5:45pm',
         speaker: 'Dr. Yves Hilpisch, CEO at The Python Quants',
-        title: 'Closing Keynote'
+        title: 'Closing Keynote',
       },
     ],
   },
@@ -41,7 +61,7 @@ const commonSchedule = {
       {
         time: '9:00am - 10:00am',
         speaker: 'Alan Descoins, CTO at Tryolabs',
-        title: 'Opening Keynote'
+        title: 'Opening Keynote',
       },
       {
         time: '10:00am - 10:40am',
@@ -50,13 +70,12 @@ const commonSchedule = {
         type: 'break',
       },
     ],
-    closing: [
-    ],
-  }
+    closing: [],
+  },
 }
 
 const tracks = {
-  'dataScience': {
+  dataScience: {
     tabHeader: 'Track 1: Data Science',
     day2: [
       {
@@ -67,7 +86,8 @@ const tracks = {
       {
         time: '11:30am - 12:00pm',
         speaker: 'David Tan',
-        title: 'How to deploy machine learning models to production (frequently and safely)',
+        title:
+          'How to deploy machine learning models to production (frequently and safely)',
       },
       {
         time: '12:00pm - 1:00pm',
@@ -78,12 +98,14 @@ const tracks = {
       {
         time: '1:00pm - 1:40pm',
         speaker: 'Loveme Felicilda, Data Scientist at Ahrefs',
-        title: 'GloVe - Python for Word Representation'
+        title: 'GloVe - Python for Word Representation',
       },
-      { 
+      {
         time: '1:45pm - 2:25pm',
-        speaker: 'Kenneth Emeka Odoh, Software Engineer at Apivio Systems / Nuri Telecoms',
-        title: 'Tracking the tracker: Time Series Analysis in Python From First Principles'
+        speaker:
+          'Kenneth Emeka Odoh, Software Engineer at Apivio Systems / Nuri Telecoms',
+        title:
+          'Tracking the tracker: Time Series Analysis in Python From First Principles',
       },
       {
         time: '2:30pm - 3:40pm',
@@ -94,24 +116,29 @@ const tracks = {
       {
         time: '3:45pm - 4:25pm',
         speaker: 'Jeanne Choo',
-        title: 'Better Care and Feeding of Machine Learning Models'
+        title: 'Better Care and Feeding of Machine Learning Models',
       },
       {
         time: '4:30pm - 5:10pm',
         speaker: 'Loryfel Nunez, Data Scientist at JP Morgan Chase & Co.',
-        title: 'Teaching Computers ABCs: A Quick Intro to Natural Language Processing'
+        title:
+          'Teaching Computers ABCs: A Quick Intro to Natural Language Processing',
       },
     ],
     day3: [
       {
         time: '10:45am - 11:25am',
-        speaker: 'Alizishaan Khatri, Machine Learning Engineer at Pivotus Ventures',
-        title: 'Detecting offensive messages using Deep Learning: A micro-service based approach'
+        speaker:
+          'Alizishaan Khatri, Machine Learning Engineer at Pivotus Ventures',
+        title:
+          'Detecting offensive messages using Deep Learning: A micro-service based approach',
       },
       {
         time: '11:30am - 12:00pm',
-        speaker: 'Markku Lepistö, Solutions Architect - APAC and Japan, Google Cloud Platform at Google',
-        title: 'Streaming data processing pipelines with Apache Beam [in Python, naturally!]'
+        speaker:
+          'Markku Lepistö, Solutions Architect - APAC and Japan, Google Cloud Platform at Google',
+        title:
+          'Streaming data processing pipelines with Apache Beam [in Python, naturally!]',
       },
       {
         time: '12:00pm - 1:00pm',
@@ -120,9 +147,11 @@ const tracks = {
         type: 'break',
       },
       {
-        time: '1:00pm - 2:25pm',        
-        speaker: 'Sarthak Deshwal, Associate Software Engineer at Expedia Group',
-        title: 'How to understand user behaviour using Multiple Linear Regression?'
+        time: '1:00pm - 2:25pm',
+        speaker:
+          'Sarthak Deshwal, Associate Software Engineer at Expedia Group',
+        title:
+          'How to understand user behaviour using Multiple Linear Regression?',
       },
       {
         time: '2:30pm - 4:25pm',
@@ -133,22 +162,24 @@ const tracks = {
       {
         time: '4:30pm - 5:15pm',
         speaker: 'Novia Listiyani Wirhaspati',
-        title: 'Implementing Reinforcement Learning for Marketing Channel Optimization with Python'
+        title:
+          'Implementing Reinforcement Learning for Marketing Channel Optimization with Python',
       },
-    ]
+    ],
   },
-  'webDevOps': {
+  webDevOps: {
     tabHeader: 'Track 2: Web Development / DevOps',
     day2: [
       {
         time: '10:45am - 11:25am',
         speaker: 'Chinab Chugh',
-        title: 'A scalable blueprint for product customization with Python 3 '
+        title: 'A scalable blueprint for product customization with Python 3 ',
       },
       {
         time: '11:30am - 12:00pm',
         speaker: 'Wilson Tjhi',
-        title: 'Writing robust, readable, and maintainable concurrent programs in Python'
+        title:
+          'Writing robust, readable, and maintainable concurrent programs in Python',
       },
       {
         time: '12:00pm - 1:00pm',
@@ -159,12 +190,12 @@ const tracks = {
       {
         time: '1:00pm - 1:40pm',
         speaker: 'Ernst Haagsman',
-        title: 'Dockerizing Django'
+        title: 'Dockerizing Django',
       },
       {
         time: '1:45pm - 2:30pm',
         speaker: 'Galuh Sahid, Data Engineer at Midtrans',
-        title: 'Build a Data-Driven Web App That Everyone Can Use'
+        title: 'Build a Data-Driven Web App That Everyone Can Use',
       },
       {
         time: '2:30pm',
@@ -175,24 +206,25 @@ const tracks = {
       {
         time: '3:45pm - 4:25pm',
         speaker: 'Mohammed A Imran, Senior Security Engineer at Zendesk',
-        title: 'Devil is in the details: Microservices, security and DevOps'
+        title: 'Devil is in the details: Microservices, security and DevOps',
       },
       {
         time: '4:30pm - 5:10pm',
-        speaker: 'Tang Ing Wei, Lead System Engineer at ROKKI Avionics Pte. Ltd.',
-        title: 'Taming System with Python: A Sysadmin perspective'
+        speaker:
+          'Tang Ing Wei, Lead System Engineer at ROKKI Avionics Pte. Ltd.',
+        title: 'Taming System with Python: A Sysadmin perspective',
       },
     ],
     day3: [
       {
         time: '10:45am - 11:25am',
         speaker: 'Graham Dumbleton',
-        title: 'Deploying Applications using Kubernetes'
+        title: 'Deploying Applications using Kubernetes',
       },
       {
         time: '11:30am - 12:00pm',
         speaker: 'TBD',
-        title: '100 Offer Talk'
+        title: '100 Offer Talk',
       },
       {
         time: '12:00pm - 1:00pm',
@@ -203,7 +235,7 @@ const tracks = {
       {
         time: '1:00pm - 2:25pm',
         speaker: 'Jonathan Reiter',
-        title: 'Testing Complex Python Dependencies With Docker Containers'
+        title: 'Testing Complex Python Dependencies With Docker Containers',
       },
       {
         time: '2:30pm - 3:45pm',
@@ -213,23 +245,25 @@ const tracks = {
       },
       {
         time: '3:45pm - 4:30pm',
-        speaker: 'Denys Makogon, Principal Software Development Engineer at Oracle',
-        title: 'Applying serverless architecture pattern to distributed data processing'
+        speaker:
+          'Denys Makogon, Principal Software Development Engineer at Oracle',
+        title:
+          'Applying serverless architecture pattern to distributed data processing',
       },
-    ]
+    ],
   },
-  'others': {
+  others: {
     tabHeader: 'Track 3: Other',
     day2: [
       {
         time: '10:45am - 11:25am',
         speaker: 'Abhishek Pandey',
-        title: 'Run your ICO using Python'
+        title: 'Run your ICO using Python',
       },
       {
         time: '11:30am - 12:00pm',
         speaker: 'Jonas Obrist, Software Engineer at HDE Inc.',
-        title: 'Artisanal Async Adventures'
+        title: 'Artisanal Async Adventures',
       },
       {
         time: '12:00pm - 1:00pm',
@@ -240,12 +274,13 @@ const tracks = {
       {
         time: '1:00pm - 1:40pm',
         speaker: 'Ram, Director/Co-founder at Zentropy Pvt Ltd',
-        title: 'Practical Python Design Patterns'
+        title: 'Practical Python Design Patterns',
       },
       {
         time: '1:45pm - 2:25pm',
         speaker: 'Satish Shankar, Data and AI Lead at Manulife',
-        title: 'Crypto-AI: A Guide to Building Private and Decentralized AI Systems by Mixing Cryptography, Blockchains and AI.'
+        title:
+          'Crypto-AI: A Guide to Building Private and Decentralized AI Systems by Mixing Cryptography, Blockchains and AI.',
       },
       {
         time: '2:30pm - 3:45pm',
@@ -256,24 +291,24 @@ const tracks = {
       {
         time: '3:45pm - 4:25pm',
         speaker: 'Terry Yin, Programmer at Odd-e',
-        title: 'Test-Driven Development of a Static Code Analyzer in Python'
+        title: 'Test-Driven Development of a Static Code Analyzer in Python',
       },
       {
         time: '4:30pm - 5:10pm',
         speaker: 'Manabu Terada',
-        title: 'Introduce syntax and history of Python from 2.4 to 3.6'
+        title: 'Introduce syntax and history of Python from 2.4 to 3.6',
       },
     ],
     day3: [
       {
         time: '10:45am - 11:25am',
         speaker: 'Narendran R, Senior Software Consultant at Tarka',
-        title: 'Concurrency vs Parallelism'
+        title: 'Concurrency vs Parallelism',
       },
       {
         time: '11:30am - 12:00pm',
         speaker: 'Vigneshwer Dhinakaran, Mozilla TechSpeaker',
-        title: 'Pumping up Python modules using Rust'
+        title: 'Pumping up Python modules using Rust',
       },
       {
         time: '12:00pm - 1:00pm',
@@ -284,12 +319,13 @@ const tracks = {
       {
         time: '1:00pm - 1:40pm',
         speaker: 'Aleksandr Koshkin, Senior Developer at Positive Technologies',
-        title: 'Inside a PyPy JIT: is that even legal?'
+        title: 'Inside a PyPy JIT: is that even legal?',
       },
       {
         time: '1:45pm - 2:30pm',
-        speaker: 'Tsung-Hsien Lee, Staff Software Engineer at Toyota Research Institute',
-        title: 'Elements of Programming Interviews in Python'
+        speaker:
+          'Tsung-Hsien Lee, Staff Software Engineer at Toyota Research Institute',
+        title: 'Elements of Programming Interviews in Python',
       },
       {
         time: '2:30pm - 3:45pm',
@@ -300,70 +336,67 @@ const tracks = {
       {
         time: '3:45pm - 4:25pm',
         speaker: 'TBD',
-        title: 'TBD'
+        title: 'TBD',
       },
       {
         time: '4:30pm - 5:15pm',
         speaker: 'Sony Valdez, Python Philippines',
-        title: 'Game Programming with Python'
-      },      
-    ]
+        title: 'Game Programming with Python',
+      },
+    ],
   },
-};
+}
 
 class ConferenceSchedulePage extends React.Component {
-
   constructor(props) {
     super(props)
 
-    this.toggle = this.toggle.bind(this);
+    this.toggle = this.toggle.bind(this)
 
-    const val = Object.keys(tracks).map((key) => {
-      return key;
-    });
+    const val = Object.keys(tracks).map(key => {
+      return key
+    })
 
-    console.log(val);
     this.state = {
       activeTab: 'dataScience',
     }
   }
-  
+
   toggle(tab) {
     if (this.state.activeTab !== tab) {
       this.setState({
-        activeTab: tab
-      });
+        activeTab: tab,
+      })
     }
   }
 
   renderSchedule(talks) {
-    return talks.map((element) => {
-
+    return talks.map(element => {
       if (element.type === 'break') {
         return (
-          <tr>
+          <tr key={element.title}>
             <td>
               <Row>
-                <Col sm='3'>
+                <Col xs="12" sm="3">
                   {element.time}
                 </Col>
-                <Col sm='9'>
+                <Col xs="12" sm="9">
                   <b>{element.title}</b>
                 </Col>
               </Row>
             </td>
           </tr>
-        );
+        )
       }
-      
+
       return (
-        <tr>
+        <tr key={element.title}>
           <td>
-            <Row>  
-              <Col sm='3'>
-                {element.time}
+            <Row>
+              <Col xs="12" sm="3">
+                <p>{element.time}</p>
               </Col>
-              <Col sm='9'>
+              <Col xs="12" sm="9">
                 <b>{element.title}</b>
                 <br />
                 <i>by {element.speaker}</i>
@@ -371,96 +404,89 @@ class ConferenceSchedulePage extends React.Component {
             </Row>
           </td>
         </tr>
-      );
-      }                  
-    )
+      )
+    })
   }
 
   renderTab(day) {
     return (
       <div>
-    <Nav tabs>
-      {
-        Object.keys(tracks).map((key) => {
-          return (
-          <NavItem>
-            <NavLink
-              key={key}
-              className={classnames({ active: this.state.activeTab ===  key})}
-              onClick={() => { this.toggle(key); }}
-            >
-            <b>{tracks[key].tabHeader}</b>
-            </NavLink>
-          </NavItem>);
-        })
-      }
-    </Nav>
-    <TabContent activeTab={this.state.activeTab}>
-      {
-        Object.keys(tracks).map((key) => 
-          <TabPane tabId={key}>
-            <Table striped>
-            <tbody>
-            <br />
-            {
-              this.renderSchedule(tracks[key][day])
-            }
-            </tbody>
-            </Table>
-          </TabPane>
-        )
-      }    
-    </TabContent>
-    </div>
-    );
+        <Nav tabs>
+          {Object.keys(tracks).map(key => {
+            return (
+              <NavItem key={key}>
+                <NavLink
+                  key={key}
+                  className={classnames({
+                    active: this.state.activeTab === key,
+                  })}
+                  style={{
+                    color: this.state.activeTab === key ? '#2a56a2' : '#9eb2da',
+                  }}
+                  onClick={() => {
+                    this.toggle(key)
+                  }}
+                >
+                  <b>{tracks[key].tabHeader}</b>
+                </NavLink>
+              </NavItem>
+            )
+          })}
+        </Nav>
+        <TabContent activeTab={this.state.activeTab}>
+          {Object.keys(tracks).map(key => (
+            <TabPane tabId={key} key={key}>
+              <Table striped>
+                <tbody>{this.renderSchedule(tracks[key][day])}</tbody>
+              </Table>
+            </TabPane>
+          ))}
+        </TabContent>
+      </div>
+    )
   }
 
   render() {
     return (
-      <div className='container'>
+      <div className="container">
         <Helmet>
           <title>Conference Schedule | PyCon APAC 2018</title>
         </Helmet>
-        
+
         <ContentCard>
           <h1>Conference Schedule</h1>
+          <Button href="#day2">Day 2</Button>{' '}
+          <Button href="#day3">Day 3</Button>
+          {Object.keys(commonSchedule).map(day => {
+            return (
+              <div key={day}>
+                <br />
+                <h4 id={day}>{commonSchedule[day].label}</h4>
+                <br />
+                <Table striped>
+                  <tbody>
+                    {this.renderSchedule(commonSchedule[day].opening)}
+                  </tbody>
+                </Table>
 
-          <Button href='#day2'>Day 2</Button> <Button href='#day3'>Day 3</Button>
-          
-          {
-            Object.keys(commonSchedule).map(day => {
-              return (
-                <div>
-                  <br />
-                  <Card body>
-                    <h4 id={day}>{commonSchedule[day].label}</h4>
-                    <br />
-                    <Table striped>
-                      <tbody>
-                      {this.renderSchedule(commonSchedule[day].opening)}
-                      </tbody>
-                    </Table>
+                <br />
 
-                    <br />
-                    
-                      {this.renderTab(day)}
+                {this.renderTab(day)}
 
-                    <br />
+                <br />
 
-                    <Table striped>
-                      <tbody>
-                      {this.renderSchedule(commonSchedule[day].closing)}
-                      </tbody>
-                    </Table>
-                  </Card>
-                </div>
-              )
-            })
-          }  
+                <Table striped>
+                  <tbody>
+                    {this.renderSchedule(commonSchedule[day].closing)}
+                  </tbody>
+                </Table>
+              </div>
+            )
+          })}
         </ContentCard>
       </div>
     )
   }
 }
 
-export default ConferenceSchedulePage;
+export default ConferenceSchedulePage
