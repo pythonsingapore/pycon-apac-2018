b1edf54d176809d17460bb4bf8bf2d7d028c3bad
Martin Brochhaus <mbrochh@gmail.com>
2018-05-20 10:50:46 +0800

    Adds 100offer and Solacetech to sponsors

--- src/layouts/Sponsors.js
+++ src/layouts/Sponsors.js
@@ -4,15 +4,18 @@ import { Row, Col } from 'reactstrap'
 import ContentCard from '../components/ContentCard'
 import Sponsor from '../components/Sponsor'
 
-import SponsorJetbrainsPng from '../images/sponsor-jetbrains.png'
-import SponsorZendeskPng from '../images/sponsor-zendesk.png'
-import SponsorTravelokaPng from '../images/sponsor-traveloka.png'
-import SponsorGandiPng from '../images/sponsor-gandi.png'
-import SponsorThoughtworksPng from '../images/sponsor-thoughtworks.png'
-import SponsorTheartlingPng from '../images/sponsor-theartling.png'
+import SponsorAcronisPng from '../images/sponsor-acronis.png'
 import SponsorBitlabstudioPng from '../images/sponsor-bitlabstudio.png'
-import SponsorJubliaJpg from '../images/sponsor-jublia.jpg'
+import SponsorGandiPng from '../images/sponsor-gandi.png'
 import SponsorHDEPng from '../images/sponsor-hde.png'
+import SponsorJetbrainsPng from '../images/sponsor-jetbrains.png'
+import SponsorJubliaJpg from '../images/sponsor-jublia.jpg'
+import SponsorSolacetechPng from '../images/sponsor-solacetech.png'
+import SponsorTheartlingPng from '../images/sponsor-theartling.png'
+import SponsorThoughtworksPng from '../images/sponsor-thoughtworks.png'
+import SponsorTravelokaPng from '../images/sponsor-traveloka.png'
+import SponsorZendeskPng from '../images/sponsor-zendesk.png'
+import Sponsor100OfferPng from '../images/sponsor-100offer.png'
 
 const Sponsors = () => (
   <div className="container">
@@ -76,9 +79,27 @@ const Sponsors = () => (
           />
         </Col>
       </Row>
+      <Row className="mt-5 justify-content-center">
+        <Col sm="4">
+          <Sponsor
+            name="100 Offer"
+            url="https://100offer.com/"
+            image={Sponsor100OfferPng}
+            tier="Silver"
+          />
+        </Col>
+      </Row>
       <h1 className="mt-5">Silver Sponsors</h1>
       <hr />
       <Row>
+        <Col sm="4">
+          <Sponsor
+            name="Solacetech"
+            url="https://www.solacetech.com.sg/"
+            image={SponsorSolacetechPng}
+            tier="Silver"
+          />
+        </Col>
         <Col sm="4">
           <Sponsor
             name="The Artling"
@@ -95,11 +116,21 @@ const Sponsors = () => (
             tier="Silver"
           />
         </Col>
+      </Row>
+      <Row className="mt-5 justify-content-center">
         <Col sm="4">
           <Sponsor
             name="Bitlab Studio"
             url="https://www.bitlabstudio.com/"
             image={SponsorBitlabstudioPng}
+            tier="Silver"
+          />
+        </Col>
+        <Col sm="4">
+          <Sponsor
+            name="Acronis"
+            url="https://www.acronis.com/"
+            image={SponsorAcronisPng}
             tier="Silver"
           />
         </Col>
